748e063432b5bed47c5a13d23901887910df08fa
William Lupinacci <will.lupinacci@gmail.com>
2024-12-30 19:57:55 -0500

    Simple text output
    This works on an external USB stick by copying bootx64.efi
    to the partition of a properly formatted stick.  Have yet to test
    QEMU.

--- assemble-link-create-image.sh
+++ assemble-link-create-image.sh
@@ -1,8 +1,12 @@
-# our bootloader (BOOTX64.EFI) is a PE executable in a FAT image ! lmao
+# See https://web.archive.org/web/20190706212328/https://gist.github.com/AdrianKoshka/5b6f8b6803092d8b108cda2f8034539a
+# for creating a UEFI USB properly, then move bootx64.efi to /efi/usb on the first USB partition
+
+# This probably works for qemu though, our bootloader (BOOTX64.EFI) is a PE executable in a FAT image ! lmao
 
 # assemble
 nasm -f win64 bootloader.asm -o bootloader.obj
-# link with PE header to create PE executable
+# link with PE header (DOS header + COFF header + "optional" PE header) to create PE executable
+# my code starts at offset 0x200 in BOOTX64.EFI, and the program is filled with 0xCC till 1024 bytes for some reason
 clang --target=x86_64-unknown-windows -nostdlib -Wl,-entry:start -Wl,-subsystem:efi_application -fuse-ld=lld -o BOOTX64.EFI bootloader.obj
 # create vfat image to copy PE executable into
 dd if=/dev/zero of=bootloader.img bs=1M count=1
--- bootloader.asm
+++ bootloader.asm
@@ -1,3 +1,5 @@
+; See https://web.archive.org/web/20190706212328/https://gist.github.com/AdrianKoshka/5b6f8b6803092d8b108cda2f8034539a
+; for creating a UEFI USB properly, then move bootx64.efi to /efi/usb on the first USB partition
 ;64-bit code
 bits 64
 ;relative adresses
@@ -5,8 +7,88 @@ default rel
 
 section .text
 global start
+; start of code
 
+%macro UINTN 0
+    RESQ 1
+    alignb 8
+%endmacro
+
+%macro UINT32 0
+    RESD 1
+    alignb 4
+%endmacro
+
+%macro UINT64 0
+    RESQ 1
+    alignb 8
+%endmacro
+
+%macro EFI_HANDLE 0
+    RESQ 1
+    alignb 8
+%endmacro
+
+%macro POINTER 0
+    RESQ 1
+    alignb 8
+%endmacro
+
+; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G8.1001729
+struc EFI_TABLE_HEADER
+    .Signature  UINT64
+    .Revision   UINT32
+    .HeaderSize UINT32
+    .CRC32      UINT32
+    .Reserved   UINT32
+endstruc
+
+; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G8.1001773
+struc EFI_SYSTEM_TABLE
+    .Hdr                  RESB EFI_TABLE_HEADER_size
+    .FirmwareVendor       POINTER
+    .FirmwareRevision     UINT32
+    .ConsoleInHandle      EFI_HANDLE
+    .ConIn                POINTER
+    .ConsoleOutHandle     EFI_HANDLE
+    .ConOut               POINTER
+    .StandardErrorHandle  EFI_HANDLE
+    .StdErr               POINTER
+    .RuntimeServices      POINTER
+    .BootServices         POINTER
+    .NumberOfTableEntries UINTN
+    .ConfigurationTable   POINTER
+endstruc
+
+; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G16.1016807
+struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+    .Reset             POINTER
+    .OutputString      POINTER
+    .TestString        POINTER
+    .QueryMode         POINTER
+    .SetMode           POINTER
+    .SetAttribute      POINTER
+    .ClearScreen       POINTER
+    .SetCursorPosition POINTER
+    .EnableCursor      POINTER
+    .Mode              POINTER
+endstruc
 .first_byte db 0x44
 .second_byte db 0x22
+message db __utf16__ `\r\n\nyo people :)\n\nHow are you all?\0`
 
 start:
+	sub rsp, 40 ; "specially misaligned stack"
+	mov rcx, [rdx + EFI_SYSTEM_TABLE.ConOut]
+	mov rdx, message
+	call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+	cmp rax,0 ;if EFI_SUCCESS
+	jne .error
+
+	add rsp, 40
+	jmp $
+.error:
+
+
+section .reloc ;UEFI supposedly requires this, even if empty
+
