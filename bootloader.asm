fd7ecec42bfe2b84bff3cfe0e43e7856851daa66
William Lupinacci <will.lupinacci@gmail.com>
2025-01-01 15:21:49 -0500

    Properly pad and align strucs

--- bootloader.asm
+++ bootloader.asm
@@ -7,72 +7,6 @@ default rel
 
 section .text
 global start
-; start of code
-
-%macro UINTN 0
-    RESQ 1
-    alignb 8
-%endmacro
-
-%macro UINT32 0
-    RESD 1
-    alignb 4
-%endmacro
-
-%macro UINT64 0
-    RESQ 1
-    alignb 8
-%endmacro
-
-%macro EFI_HANDLE 0
-    RESQ 1
-    alignb 8
-%endmacro
-
-%macro POINTER 0
-    RESQ 1
-    alignb 8
-%endmacro
-
-; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G8.1001729
-struc EFI_TABLE_HEADER
-    .Signature  UINT64
-    .Revision   UINT32
-    .HeaderSize UINT32
-    .CRC32      UINT32
-    .Reserved   UINT32
-endstruc
-
-; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G8.1001773
-struc EFI_SYSTEM_TABLE
-    .Hdr                  RESB EFI_TABLE_HEADER_size
-    .FirmwareVendor       POINTER
-    .FirmwareRevision     UINT32
-    .ConsoleInHandle      EFI_HANDLE
-    .ConIn                POINTER
-    .ConsoleOutHandle     EFI_HANDLE
-    .ConOut               POINTER
-    .StandardErrorHandle  EFI_HANDLE
-    .StdErr               POINTER
-    .RuntimeServices      POINTER
-    .BootServices         POINTER
-    .NumberOfTableEntries UINTN
-    .ConfigurationTable   POINTER
-endstruc
-
-; see http://www.uefi.org/sites/default/files/resources/UEFI Spec 2_7_A Sept 6.pdf#G16.1016807
-struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
-    .Reset             POINTER
-    .OutputString      POINTER
-    .TestString        POINTER
-    .QueryMode         POINTER
-    .SetMode           POINTER
-    .SetAttribute      POINTER
-    .ClearScreen       POINTER
-    .SetCursorPosition POINTER
-    .EnableCursor      POINTER
-    .Mode              POINTER
-endstruc
 
 
 ; Start of program
@@ -90,8 +24,18 @@ start:
 	mov QWORD [efi_handle], rcx ; Save EFI_HANDLE
 	mov QWORD [system_table_ptr], rdx ; Save system table pointer
 
-	; Clear screen
-	call clear_screen
+	jmp $
+	mov eax, EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_size
+	mov eax, EFI_SYSTEM_TABLE_size
+	mov eax, EFI_TABLE_HEADER_size
+
+	; Reset screen
+	mov rcx, 0 ;argument for reset screen, no verification (verification can take a long time)
+	call reset_screen
+	cmp rax, 0
+	je .reset_screen_successful_1
+	jmp error_exit
+	.reset_screen_successful_1:
 
 	; Print generic message
 	; print_string(generic_message_str)
@@ -111,6 +55,22 @@ start:
 		;mov rsp, QWORD [stack_init]
 		add rsp, 4 * 8 + 8
 		retn
+
+; sets cursor position to 0,0, clears screen to default background color, and optionally performs verification
+; of device functioning
+reset_screen: ;in: rcx (0 for no verification, 1 for verification), out: rax
+	sub rsp, 4 * 8 + 8
+
+	mov QWORD [rsp + 48], rcx ;store in shadow space for rcx lmao; https://www.scss.tcd.ie/jeremy.jones/CSU34021/2%20IA32%20+%20x64.pdf page 39
+
+	; MICROSOFT FUNCTION CALL START
+	mov rcx, QWORD [system_table_ptr] ; rcx is first argument - pointer to EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+	mov rcx, QWORD [rcx + EFI_SYSTEM_TABLE.ConOut]
+	mov rdx, QWORD [rsp + 48]; assign passed rcx to rdx - second argument - 0 for no verification, 1 for verification
+	call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.Reset]
+	; MICROSOFT FUNCTION CALL END
+
+	add rsp, 4 * 8 + 8
 
 ; clears the screen with currently set background color
 clear_screen:
@@ -162,7 +122,7 @@ fun_func:
 	sub rsp, 4 * 8 + 8
 
 	; print_string(generic_message_str)
-	mov rcx, generic_message_str
+	mov rcx, generic_message_str_two
 	call print_string
 	cmp rax, 0
 	je .fun_func_successful_1
@@ -175,15 +135,124 @@ fun_func:
 
 section .reloc ;UEFI supposedly requires this, even if empty
 
-
-
 ; "The registers Rax, Rcx Rdx R8, R9, R10, R11, and XMM0-XMM5 are volatile and are, therefore, destroyed on function calls.
 ;"The registers RBX, RBP, RDI, RSI, R12, R13, R14, R15, and XMM6-XMM15 are considered nonvolatile and must be saved and restored by a function that uses them."
 ; "https://uefi.org/specs/UEFI/2.10/02_Overview.html"
-section .data
+section .data align=16
 	stack_init dq 0
 	system_table_ptr dq 0
 	efi_handle dq 0
-	generic_message_str db __utf16__ `\r\n\nnew message two :)\n\nHow are you all?\r\n\0`
-	generic_message_str_two db __utf16__ `\r\n\ntwo new message two :)\n\nHow are you all?\r\n\0`
+
+
+section .rdata align=16
+	%macro UINTN 0
+	    RESQ 1
+	%endmacro
+
+	%macro UINT32 0
+	    RESD 1
+	%endmacro
+
+	%macro UINT64 0
+	    RESQ 1
+	%endmacro
+
+	%macro EFI_HANDLE 0
+	    RESQ 1
+	%endmacro
+
+	%macro POINTER 0
+	    RESQ 1
+	%endmacro
+
+	%macro UINTN_ALIGN 0
+	    alignb 8
+	%endmacro
+
+	%macro UINT32_ALIGN 0
+	    alignb 4
+	%endmacro
+
+	%macro UINT64_ALIGN 0
+	    alignb 8
+	%endmacro
+
+	%macro EFI_HANDLE_ALIGN 0
+	    alignb 8
+	%endmacro
+
+	%macro POINTER_ALIGN 0
+	    alignb 8
+	%endmacro
+
+	struc EFI_TABLE_HEADER ;nasm sees this as 24 bytes
+		UINT64_ALIGN
+		.Signature  UINT64
+		UINT32_ALIGN
+		.Revision   UINT32
+		UINT32_ALIGN
+		.HeaderSize UINT32
+		UINT32_ALIGN
+		.CRC32      UINT32
+		UINT32_ALIGN
+		.Reserved   UINT32
+		resb (($ - EFI_TABLE_HEADER.Signature) % 8)
+	endstruc
+
+	struc EFI_SYSTEM_TABLE ;nasm sees this as 120 bytes
+		POINTER_ALIGN
+		.Hdr                  resb EFI_TABLE_HEADER_size ;24
+		POINTER_ALIGN
+		.FirmwareVendor       POINTER ;8
+		UINT32_ALIGN
+		.FirmwareRevision     UINT32 ;4
+		EFI_HANDLE_ALIGN
+		.ConsoleInHandle      EFI_HANDLE ;8
+		POINTER_ALIGN
+		.ConIn                POINTER ;8
+		EFI_HANDLE_ALIGN
+		.ConsoleOutHandle     EFI_HANDLE ;8
+		POINTER_ALIGN
+		.ConOut               POINTER ;8
+		EFI_HANDLE_ALIGN
+		.StandardErrorHandle  EFI_HANDLE ;8
+		POINTER_ALIGN
+		.StdErr               POINTER ;8
+		POINTER_ALIGN
+		.RuntimeServices      POINTER ;8
+		POINTER_ALIGN
+		.BootServices         POINTER ;8
+		UINTN_ALIGN
+		.NumberOfTableEntries UINTN ;8
+		POINTER_ALIGN
+		.ConfigurationTable   POINTER ;8
+		resb (($ - EFI_SYSTEM_TABLE.Hdr) % 8) ;this is actually padding 4 bytes so it is working
+	endstruc
+
+	struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL ;nasm sees this as 80 bytes
+		POINTER_ALIGN
+		.Reset             POINTER
+		POINTER_ALIGN
+		.OutputString      POINTER
+		POINTER_ALIGN
+		.TestString        POINTER
+		POINTER_ALIGN
+		.QueryMode         POINTER
+		POINTER_ALIGN
+		.SetMode           POINTER
+		POINTER_ALIGN
+		.SetAttribute      POINTER
+		POINTER_ALIGN
+		.ClearScreen       POINTER
+		POINTER_ALIGN
+		.SetCursorPosition POINTER
+		POINTER_ALIGN
+		.EnableCursor      POINTER
+		POINTER_ALIGN
+		.Mode              POINTER
+		resb (($ - EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.Reset) % 8)
+	endstruc
+
+	generic_message_str db __utf16__ `\rOS-UEFI BOOTLOADER v0.1\r\n\0`
+	generic_message_str_two db __utf16__ `\rCopyright William Lupinacci\r\n\0`
 	generic_error_exit_str db __utf16__ `error encountered, exiting...\r\n\0`
--- qemu-run.sh
+++ qemu-run.sh
@@ -8,4 +8,4 @@
 qemu-system-x86_64 -cpu qemu64 \
   -drive if=pflash,format=raw,unit=0,file=./OVMF.fd,readonly=on \
   -drive if=pflash,format=raw,unit=1,file=./OVMF.fd \
-  -net none -drive file=bootloader.img,format=raw,index=0,media=disk
+  -net none -drive file=bootloader.img,format=raw,index=0,media=disk -S -s
